adaea5fc2a0ca83f97fe6563a5c9339797b85403
LAPTOP-2B0B4KAO\mingh <minghao0625@outlook.com>
2021-03-16 14:09:13 +0100

    text cleaning and word frequency count

--- Pre-process.R
+++ Pre-process.R
@@ -1,3 +1,6 @@
+library("wordcloud")
+library("quanteda")
+
 
 reviews_df <- Womens.Clothing.E.Commerce.Reviews
 
@@ -5,7 +8,7 @@ reviews_df <- Womens.Clothing.E.Commerce.Reviews
 reviews_df$text_full <- paste(reviews_df$Title,reviews_df$Review.Text,sep = " ")
 
 
-#First stage of cleaning
+#First stage of cleaning ----
 reviews_df$Review <- as.character(reviews_df$text_full)  %>% 
   tolower() %>% 
   {gsub(":( |-|o)*\\("," SADSMILE ", .)} %>%       # Find :( or :-( or : ( or :o(
@@ -27,17 +30,58 @@ reviews_df$Review <- as.character(reviews_df$text_full)  %>%
 
 reviews_df$Review
 
-# # Creating the full review from the cleaned+stemmedwords
-# j<-1
-# for (j in 1:nrow(reviews_df)) {
-#  stemmed_Review<-  anti_join((reviews_df[j,] %>% unnest_tokens(word,Review, drop=FALSE,to_lower=TRUE) ),stop_words)
-#   
-#  stemmed_Review<-(wordStem(stemmed_Review[,"word"], language = "porter"))
-# 
-#  reviews_df[j,"Review"]<-paste((stemmed_Review),collapse = " ")
-#   
-# }
-# 
-# save(reviews_df , file = "C:/Users/24493ado/OneDrive - Erasmus University Rotterdam/Documents/Documents/college/text-analytics/data/yelp_review_full_csv/yelp_academic_dataset_review_restaurants_50kstemmed.Rdata")
+#cut text into words by splitting on spaces and punctuation
+review_words <- reviews_df %>% unnest_tokens(word,Review,to_lower=TRUE) 
+print("number of words")
+nrow(review_words)
 
-```
+#Count the number of times each word occurs
+counts <- review_words %>%count(word, sort=TRUE) # sort = TRUE for sorting in descending order of n. 
+print("number of unique words after stemming and without stop words")
+nrow(counts)
+
+
+# Visualization raw ---- 
+# frequency (raw)
+counts %>% 
+  mutate(word = reorder(word,n)) %>% 
+  top_n(20, word) %>%
+  ggplot(aes(word,n)) +  
+  geom_col() + 
+  labs(x = NULL, y = "Number of occurences") + 
+  coord_flip() + 
+  theme(text = element_text(size = 17)) + 
+  ggtitle("Word Frequency Histogram")
+
+
+wordcount_all <- reviews_df %>% unnest_tokens(word,Review) %>% count(word, sort=TRUE)
+
+set.seed(1223)
+wordcloud(words = wordcount_all$word, freq = wordcount_all$n, min.freq = 1000,
+          max.words=70, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+# Stem ----
+
+data(stop_words)
+review_words_nostop <- review_words %>% 
+  anti_join(stop_words)
+counts <- review_words_nostop %>%
+  count(word, sort=TRUE)
+
+print("number of words without stop words")
+sum(counts$n)
+print("number of unique words")
+nrow(counts)
+
+# Creating the full review from the cleaned+stemmedwords
+j<-1
+for (j in 1:nrow(reviews_df)) {
+  stemmed_description<-  anti_join((reviews_df[j,] %>% unnest_tokens(word,Description, drop=FALSE,to_lower=TRUE) ),stop_words)
+  
+  stemmed_description<-(wordStem(stemmed_description[,"word"], language = "porter"))
+  
+  reviews_df[j,"Description"]<-paste((stemmed_description),collapse = " ")
+  
+}
